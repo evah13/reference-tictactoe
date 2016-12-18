db1e2dfa160122e8d75a5948af560b9d2f2bd487
evah13 <evah13@ru.is>
2016-12-18 15:18:10 +0000

    aftur

--- deployScript.sh
+++ deployScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/tictactoe_key.pem" /var/lib/jenkins/workspace/tictactoe/docker-compose.yaml ec2-user@82.221.49.193:docker-compose.yaml
+sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/tictactoe_key.pem" /var/lib/jenkins/workspace/tictactoe/docker-compose.yaml ec2-user@82.221.49.193:docker-compose.yaml
 
-sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/tictactoe_key.pem" /var/lib/jenkins/workspace/tictactoe/.env ec2-user@82.221.49.193:.env
+sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/tictactoe_key.pem" /var/lib/jenkins/workspace/tictactoe/.env ec2-user@82.221.49.193:.env
 
-sudo ssh -i "/var/lib/jenkins/workspace/tictactoe_key.pem" ec2-user@82.221.49.193 docker-compose up -d
+sudo ssh -i "/var/lib/jenkins/tictactoe_key.pem" ec2-user@82.221.49.193 docker-compose up -d
